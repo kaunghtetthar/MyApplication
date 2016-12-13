a6f409031f67a8f48dcee87a8d50e63fc474e013
kaunghtetthar <kaunghtetthar@Kaungs-MacBook-Pro.local>
2016-12-13 09:08:36 +0700

    Working for Direction
    Adding Direction java

---
+++ app/src/main/java/com/example/kaunghtetthar/myapplication/MapsDirection/DirectionFinderListener.java
@@ -0,0 +1,12 @@
+package com.example.kaunghtetthar.myapplication.MapsDirection;
+
+import java.util.List;
+
+/**
+ * Created by kaunghtetthar on 12/13/16.
+ */
+
+public interface DirectionFinderListener {
+    void onDirectionFinderStart();
+    void onDirectionFinderSuccess(List<Route> route);
+}
